7a2b5b939276510f89604b5eb7d5ca1eddf51d18
unknown <jason123jay@gmail.com>
2020-11-08 11:14:51 +0200

    brand new project

--- src/App.js
+++ src/App.js
@@ -4,69 +4,13 @@ import { Button, Col, Container, Input, Row } from "reactstrap";
 export class App extends Component {
   constructor(props) {
     super();
-    this.state = {
-      myListValues: ["text 1", "text 2"],
-      value: "",
-    };
+    this.state = {};
   }
 
-  onInputChanged = (value) => {
-    this.setState({ value: value.target.value });
-  };
-
-  onButtonCLicked = () => {
-    const values = [...this.state.myListValues, this.state.value];
-
-    this.setState({
-      myListValues: values,
-      value: "",
-    });
-  };
 
   render() {
     return (
-      <Container>
-        <div style={{ marginTop: 200 }}></div>
-        <Row>
-          <Col sm={3}></Col>
-          <Col sm={6}>
-            <h1
-              style={{
-                marginTop: 20,
-                marginBottom: 20,
-                textAlign: "center",
-                color: "white",
-              }}
-            >
-              My app
-            </h1>
-            <div style={{ justifyContent: "center" }}>
-              <Input
-                onChange={(e) => this.onInputChanged(e)}
-                style={{ textAlign: "center" }}
-                placeholder="Add texted here"
-                value={this.state.value}
-              />
-            </div>
-
-            <div style={{ textAlign: "center" }}>
-              <Button
-                onClick={() => this.onButtonCLicked()}
-                color="primary"
-                style={{ marginTop: 20, marginBottom: 20}}
-              >
-                Add
-              </Button>
-            </div>
-
-            {this.state.myListValues.map((value) => (
-              <div style= {{background: "white", color: "blue", textAlign: "center", marginBottom: 10, borderRadius: 50}}>{value}</div>
-            ))}
-            <Row></Row>
-          </Col>
-          <Col sm={3}></Col>
-        </Row>
-      </Container>
+      <div></div>
     );
   }
 }
